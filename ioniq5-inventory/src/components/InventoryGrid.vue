d7807d62e4cdc9a51ab06a6c0ca67a778b11240a
Ben Chapman <5124569+Ben-Chapman@users.noreply.github.com>
2022-01-20 17:08:33 -0500

    Rough, but fully working MVP

--- ioniq5-inventory/src/components/InventoryGrid.vue
+++ ioniq5-inventory/src/components/InventoryGrid.vue
@@ -1,5 +1,105 @@
 <template>
-  <b-container>
+  <b-container fluid>
+    <div>
+      <b-row>
+                <!-- Year -->
+        <b-col cols=2>
+          <b-form-group
+            id="form-year"
+            description="Select a Model Year"
+          >
+            <b-form-select
+              id="form-year"
+              v-model="form.year"
+              :options="yearOptions"
+              required
+            >
+            </b-form-select>
+          </b-form-group>
+        </b-col>
+
+        <!-- Model -->
+        <b-col cols=3>
+          <b-form-group
+            id="form-model"
+            description="Select a Model"
+          >
+            <b-form-select
+              id="form-model"
+              v-model="form.model"
+              :options="modelOptions"
+              required
+            >
+            </b-form-select>
+          </b-form-group>
+        </b-col>
+
+        <!-- Zip Code -->
+        <b-col cols=3>
+          <b-form-group
+            id="form-zipcode"
+            description="Enter a 5-digit US zip code"
+          >
+            <b-form-input
+              id="form-zipcode"
+              v-model="form.zipcode"
+              :state="isValidZipCode"
+              trim
+              debounce="250"
+              required
+            >
+            </b-form-input>
+          </b-form-group>
+        </b-col>
+
+        <!-- Radius -->
+        <b-col cols=2>
+          <b-form-group
+            id="form-radius"
+            description="Search Radius in Miles"
+          >
+            <b-form-input
+              id="form-radius"
+              v-model="form.radius"
+              :state="isValidRadius"
+              trim
+              debounce="250"
+              required
+            >
+            </b-form-input>
+          </b-form-group>
+        </b-col>
+        
+        <!-- Enabled Button -->
+        <div>
+            <span id="enabled-wrapper" class="d-inline-block" tabindex="0">
+              <b-button
+                v-if="validateSubmitButton"
+                id="submit-button"
+                variant="primary"
+                @click="getCurrentInventory()"
+                >Submit</b-button>
+            </span>
+        </div>
+
+          <!-- Disabled Button -->
+          <div>
+            <span id="disabled-wrapper" class="d-inline-block" tabindex="0">
+              <b-button
+                v-if="validateSubmitButton == false"
+                disabled
+                id="invalid-submit-button"
+                variant="outline-primary"
+                @click="getCurrentInventory()"
+                >Submit</b-button>
+              </span>
+            <b-tooltip target="disabled-wrapper" triggers="hover">
+                Please enter {{ invalidFormMessage() }}
+              </b-tooltip>
+          </div>
+
+    </b-row>
+    </div>
     <b-row>
       <b-table striped hover
         :items="this.inventory"
@@ -12,7 +112,8 @@
 <script>
 export default {
   mounted() {
-    this.getCurrentInventory()
+    // this.getCurrentInventory()
+    // this.logMe()
   },
   data() {
     return {
@@ -28,16 +129,33 @@ export default {
         { key: 'trimDesc', label: 'Trim Level', sortable: true, sortDirection: 'desc'},
         // { key: 'deliveryDate', label: 'Planned Delivery Date', sortable: true, sortDirection: 'desc'},
       ],
+
+      modelOptions: [
+         { value: 'IONIQ5', text: 'Ioniq5' },
+         { value: 'KONA', text: 'Kona'}
+      ],
+
+      yearOptions: [
+         { value: '2022', text: '2022' },
+      ],
+
+      form: {
+        zipcode: '',
+        year: '2022',
+        model: 'IONIQ5',
+        radius: '',
+      }
       
-    }
+    } // End of return
   },
   methods: {
     async getCurrentInventory() {
+      // console.log(this.form.zipcode + ' ' + this.form.radius)
       const response = await fetch('http://127.0.0.1:5000/inventory?' + new URLSearchParams({
-          zip: '07040',
-          year: '2022',
-          model: 'IONIQ5',
-          radius: '50',
+          zip: this.form.zipcode,
+          year: this.form.year,
+          model: this.form.model,
+          radius: this.form.radius,
         }),
         {
         method: 'GET',
@@ -45,12 +163,52 @@ export default {
         })
       
       this.inventory = await response.json();
-    }
+    }, 
+
+    invalidFormMessage() {
+      if (this.isValidZipCode != true) {
+        if (this.isValidRadius != true) {
+          return 'a valid zip code and a search radius.'
+        }
+      }
+      if (this.isValidZipCode != true) {
+        return 'a valid zip code.'
+      }
+      if (this.isValidRadius != true) {
+        return 'a search radius.'
+      }      
+    },
   }, // methods
 
   computed: {
-  }, // computed
-}  // default
+    isValidZipCode() {
+      // Hide the error indicator when this field is blank
+      if(this.form.zipcode.length == 0) {
+          return null
+      }
+      return /^\d{5}(-\d{4})?$/.test(this.form.zipcode)
+    },
+
+    isValidRadius() {
+      // Hide the error indicator when this field is blank
+      if(this.form.radius.length == 0) {
+          return null
+      }
+      return /^\d{1,3}$/.test(this.form.radius)
+    },
+
+    validateSubmitButton() {
+      if (this.form.zipcode && this.form.year && this.form.model && this.form.radius != '') {
+        if (this.isValidZipCode && this.isValidRadius) {
+          return true
+        }
+      }
+      return false
+    },
+  },  // End of computed
+  watch: {
+  }, // End of watch
+}  // End of default
 
 
 </script>
