b70208808c30a4858820a02513b78fa573957ba1
Ben Chapman <5124569+Ben-Chapman@users.noreply.github.com>
2022-02-04 17:40:49 -0500

    Support for KONA EV

--- ioniq5-inventory/src/components/InventoryGrid.vue
+++ ioniq5-inventory/src/components/InventoryGrid.vue
@@ -246,7 +246,7 @@
         modelOptions: [
           { value: 'IONIQ5', text: 'Ioniq5' },
           { value: 'Sonata-Hev', text: 'Sonata Hybrid'},
-          //  { value: 'KONA', text: 'Kona'}
+           { value: 'Kona-Ev', text: 'KONA Electric'}
         ],
 
         yearOptions: [
