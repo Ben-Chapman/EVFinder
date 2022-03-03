1fa5fc5d7fd28e59e7b911e4b7d3241684e862c1
Ben Chapman <5124569+Ben-Chapman@users.noreply.github.com>
2022-03-03 14:21:47 -0500

    Initial commit

---
+++ app/src/components/Kia.vue
@@ -0,0 +1,17 @@
+<template>
+  
+</template>
+
+<script>
+export default {
+
+}
+  // const mapping = {
+  //   'modelName': 'modelModel',
+  //   'year': 'yearYear',
+  // }
+</script>
+
+<style>
+
+</style>
