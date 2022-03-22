f5160410da3431a85a46cb1d4d62bf49a69c2215
Ben Chapman <5124569+Ben-Chapman@users.noreply.github.com>
2022-03-22 16:54:54 -0400

    Skeleton for status filter

--- app/src/json_mappings/kia.js
+++ app/src/json_mappings/kia.js
@@ -19,7 +19,7 @@ And more specifically:
   'trimDesc': 'edwTrim',
   'drivetrainDesc': 'drivetrainDescription',
   'price': 'msrp',
-  'PlannedDeliveryDate': '',
+  'PlannedDeliveryDate': 'inventoryStatus',
   'distance': 'range',
   'vin-with-more-details': '',
   }
