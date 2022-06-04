35a97e277e1d7c6c33df335830c80a4c8328b6b7
Ben Chapman <5124569+Ben-Chapman@users.noreply.github.com>
2022-06-04 09:25:42 -0400

    Bugfix

--- app/build.sh
+++ app/build.sh
@@ -31,13 +31,13 @@ git status -s | grep 'package-lock.json'
 if [ $? -eq 0 ]; then
   INSTALLED_VER=$(echo $UPDATE_BROWSERSLIST |grep -i "Installed Version" |awk '{print $3}')
   LATEST_VER=$(echo $UPDATE_BROWSERSLIST |grep -i "Latest version" |awk '{print $3}')
-  git_stage "package-lock.json" "Build: Update Browserslist from ${INSTALLED_VER} to ${LATEST_VER}"
+  git commit -am "Build: Update Browserslist from ${INSTALLED_VER} to ${LATEST_VER}"
 fi
 
 # Update the NPM package version from the git tag version
 VER=$(echo $TAG_NAME |sed -e 's/v//g') # Removing v from tag name v1.x.x -> 1.x.x
 npm version ${VER} -m "Build: Version bump to %s"
-git_stage "package*" "Build: Version bump to ${VER}"
+git commit -am "Build: Version bump to ${VER}"
 
 # Push changes to Github
 echo "Pushing changes to Github..."
