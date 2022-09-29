05cc178c934716393a394c935b6cfaa62cc13134
Ben Chapman <5124569+Ben-Chapman@users.noreply.github.com>
2022-09-28 21:34:31 -0400

    Adjusting spacing for the VIN detail section

--- app/src/components/InventoryTable.vue
+++ app/src/components/InventoryTable.vue
@@ -143,11 +143,11 @@
                   v-for="(item, key) in vinDetail[row.item.vin]" :key=key
                 >
                 <!-- We're displaying the Dealer URL above, don't display it here -->
-                  <b-col cols=8 md=4 v-if="key != 'DI'">
-                    <b-list-group-item class="border-0 py-1"><b>{{ key }}</b></b-list-group-item>
+                  <b-col cols=7 md=4 class="px-0" v-if="key != 'DI'">
+                    <b-list-group-item class="border-0 py-1 px-0"><b>{{ key }}</b></b-list-group-item>
                   </b-col>
                   <div v-if="key != 'DI'">
-                    <b-list-group-item class="border-0 py-1">{{ item }}</b-list-group-item>
+                    <b-list-group-item class="border-0 py-1 px-0">{{ item }}</b-list-group-item>
                   </div>
               </b-list-group>
               
