6606aff1237965c4509e18490e46cfe4603dd8be
Ben Chapman <5124569+Ben-Chapman@users.noreply.github.com>
2022-10-03 21:39:11 -0400

    Dynamic background hero images

--- app/src/App.vue
+++ app/src/App.vue
@@ -1,7 +1,12 @@
 <template>
-  <b-container fluid class="d-flex flex-column justify-content-center min-vh-100">
+  <b-container
+    fluid
+    class="d-flex flex-column justify-content-center min-vh-100 hero-image"
+    :style="{backgroundImage: `url('${randomHeroImage['imageUrl']}')`}"
+    >
       <FormSelectors/>
     <b-row class="flex-fill">
+      <!-- <HomepageImageGrid/> -->
       <InventoryTable/>
     </b-row>
     <b-row>
@@ -13,6 +18,7 @@
 <script>
 import Footer from './components/Footer.vue'
 import FormSelectors from './components/FormSelectors.vue'
+// import HomepageImageGrid from './components/HomepageImageGrid.vue'
 import InventoryTable from './components/InventoryTable.vue'
 import {version} from '../package.json'
 
@@ -33,8 +39,51 @@ export default {
   components: {
     Footer,
     FormSelectors,
+    // HomepageImageGrid,
     InventoryTable,
-},
+  },
+
+  computed: {
+    randomHeroImage() {
+      // https://stackoverflow.com/questions/42872002/in-vue-js-component-how-to-use-props-in-css/52280182#52280182
+      const vehicleImages = [
+        {
+          imageName: "2022-gv60.jpeg",
+          overlayText: "2022 Genesis GV60"
+        },
+        {
+          imageName: "2023-id.4.jpg",
+          overlayText: "2023 Volkswagen ID.4"
+        },
+        {
+          imageName: "2022-ioniq5.jpeg",
+          overlayText: "2022 Hyundai Ioniq 5"
+        },
+        {
+          imageName: "2023-ev6.jpeg",
+          overlayText: "2023 Kia EV6"
+        },
+        {
+          imageName: "2023-kona-electric.jpeg",
+          overlayText: "2023 Hyundai Kona Electric"
+        },
+        {
+          imageName: "2023-niro-ev.jpeg",
+          overlayText: "2023 Kia Niro EV"
+        },
+      ]
+      
+      // Random int which will be used to select an element from vehicleImages
+      const rand = Math.floor(Math.random() * (vehicleImages.length))
+
+      const relativeImagePath = "hero_images"
+
+      return {
+        "imageUrl": relativeImagePath.concat('/', vehicleImages[rand].imageName),
+        "title": vehicleImages[rand].overlayText
+      }
+    }
+  },
 
   // Vue.head config here
   head: {
@@ -64,6 +113,10 @@ container
     padding-right: 15px;
     padding-left: 15px;
     margin-right: auto;
-    margin-left: auto;
+    margin-left: auto;   
+}
+
+.hero-image {
+  background-size: cover;
 }
 </style>
