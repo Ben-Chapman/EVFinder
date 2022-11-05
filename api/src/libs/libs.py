0d47ab83a8eac5659b3a2aeb636781e56a782106
Joel Gomez <joel@blamejoel.com>
2022-11-05 02:51:07 -0700

    Adds API support for Chevrolet: Bolt EV, Bolt EUV

--- api/src/libs/libs.py
+++ api/src/libs/libs.py
@@ -50,6 +50,8 @@ def validate(validation_type, validation_data):
         'GV60',  # Genesis GV60
         'ElectrifiedG80',  # Genesis Electrified G80
         'ID.4', # VW ID.4
+        'Bolt EV', # Chevrolet Bolt EV
+        'Bolt EUV', # Chevrolet Bolt EUV
         ]
       valid_radii = [1, 999]
       valid_vins = []
