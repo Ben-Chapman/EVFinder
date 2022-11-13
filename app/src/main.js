5e476343652153e67e0a0c2006457b609576539e
Ben <5124569+Ben-Chapman@users.noreply.github.com>
2022-11-13 17:53:01 -0500

    Removing the Vue Head package

--- app/src/main.js
+++ app/src/main.js
@@ -1,5 +1,4 @@
 import Vue from 'vue'
-import VueHead from 'vue-head'
 import { VuePlausible } from 'vue-plausible'
 
 import App from './App.vue'
@@ -70,8 +69,6 @@ Vue.component('BIconGithub', BIconGithub)
 // Import Bootstrap an BootstrapVue CSS files (order is important)
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
-
-Vue.use(VueHead)
 
 Vue.use(VuePlausible, {
   domain: 'theevfinder.com',
