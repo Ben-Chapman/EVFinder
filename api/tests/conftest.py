3fca149ba9500169fb8d9a16817aa62f94a7c519
Ben <5124569+Ben-Chapman@users.noreply.github.com>
2022-11-17 13:06:33 -0500

    Adding Flask webserver startup fixture

--- api/tests/conftest.py
+++ api/tests/conftest.py
@@ -1,36 +1,25 @@
 # flake8: noqa
-
+import os
 import pytest
 import subprocess
 import time
 
-# https://lyz-code.github.io/blue-book/pytest_httpserver/
-# @pytest.fixture(scope="session", autouse=True)
-# def start_web_server():
-#     # https://til.simonwillison.net/pytest/subprocess-server
-#     webserver = subprocess.Popen(
-#         ["python3", "/Users/ben/src/EVFinder/api/src/run.py"],
-#         stdout=subprocess.PIPE,
-#         stderr=subprocess.STDOUT,
-#     )
-#     yield webserver
 
+@pytest.fixture(scope="session", autouse=True)
+def start_web_server():
+    # https://til.simonwillison.net/pytest/subprocess-server
+    cwd = os.getcwd()
+    if "api" not in cwd.split("/")[-1]:
+        print(f"Test initialization failed. cd to EVFinder/api and retry.")
 
-# retries = 5
-# while retries > 0:
-#     conn = requests.get
-#     try:
-#         conn.request("HEAD", "/")
-#         response = conn.getresponse()
-#         if response is not None:
-#             yield webserver
-#             break
-#     except ConnectionRefusedError:
-#         time.sleep(1)
-#         retries -= 1
+    web_server = subprocess.Popen(
+        ["python3", f"{cwd}/src/run.py"],
+        stdout=subprocess.PIPE,
+        stderr=subprocess.STDOUT,
+    )
+    time.sleep(3)  # Allow time for the Flask server to startup
+    yield web_server
 
-# if not retries:
-#     raise RuntimeError("Failed to start http server")
-# else:
-#     webserver.terminate()
-#     webserver.wait()
+    print("Terminating Web Server")
+    web_server.terminate()
+    outs, errs = web_server.communicate()
