07fb13599b351755445101502abfdae37046192f
Ben <5124569+Ben-Chapman@users.noreply.github.com>
2022-11-23 10:35:03 -0500

    Prettier formatting

--- app/src/manufacturers/audi.js
+++ app/src/manufacturers/audi.js
@@ -1,68 +1,79 @@
-import { stripHTML, titleCase } from "../helpers/libs"
-import { audiInventoryMapping, audiVinMapping } from "./audiMappings"
+import { stripHTML, titleCase } from "../helpers/libs";
+import { audiInventoryMapping, audiVinMapping } from "./audiMappings";
 
-const apiBase = 'https://api.theevfinder.com'
+const apiBase = "https://api.theevfinder.com";
 
 export async function getAudiInventory(zip, year, model, radius) {
-  const geo = await getGeoFromZipcode(zip)
+  const geo = await getGeoFromZipcode(zip);
 
-  const inventory = await fetch(apiBase + '/api/inventory/audi?' + new URLSearchParams({
-    geo: geo,
-    year: year,
-    model: model,
-    radius: radius,
-  }),
-    { method: 'GET', mode: 'cors', })
+  const inventory = await fetch(
+    apiBase +
+      "/api/inventory/audi?" +
+      new URLSearchParams({
+        geo: geo,
+        year: year,
+        model: model,
+        radius: radius,
+      }),
+    { method: "GET", mode: "cors" }
+  );
 
   if (inventory.ok) {
-    return formatAudiInventoryResults(await inventory.json())
+    return formatAudiInventoryResults(await inventory.json());
   } else {
-    return ['ERROR', inventory.status, await inventory.text()]
+    return ["ERROR", inventory.status, await inventory.text()];
   }
 }
 
 export async function getAudiVinDetail(vehicleId) {
-  const vinData = await fetch(apiBase + '/api/vin/audi?' + new URLSearchParams({
-    vehicleId: vehicleId
-  }), { method: 'GET', mode: 'cors', })
+  const vinData = await fetch(
+    apiBase +
+      "/api/vin/audi?" +
+      new URLSearchParams({
+        vehicleId: vehicleId,
+      }),
+    { method: "GET", mode: "cors" }
+  );
 
   if (vinData.ok) {
-    return formatAudiVinResults(await vinData.json())
+    return formatAudiVinResults(await vinData.json());
   } else {
-    return ['ERROR', vinData.status, vinData.text]
+    return ["ERROR", vinData.status, vinData.text];
   }
 }
 
 async function getGeoFromZipcode(zip) {
-  const osmApi = "https://nominatim.openstreetmap.org/search?"
+  const osmApi = "https://nominatim.openstreetmap.org/search?";
 
-  const geo = await fetch(osmApi + new URLSearchParams({ postalcode: zip, country: "US", format: "json" }),
-    { method: 'GET', mode: 'cors', })
-
+  const geo = await fetch(
+    osmApi +
+      new URLSearchParams({ postalcode: zip, country: "US", format: "json" }),
+    { method: "GET", mode: "cors" }
+  );
 
   if (geo.ok) {
-    const mapData = await geo.json()
+    const mapData = await geo.json();
 
     // The Audi API expects the lat/long to be provided as latitude_longitude
-    return `${mapData[0].lat}_${mapData[0].lon}`
+    return `${mapData[0].lat}_${mapData[0].lon}`;
   } else {
-    return ['ERROR', geo.status, geo.text]
+    return ["ERROR", geo.status, geo.text];
   }
 }
 
 function formatAudiInventoryResults(input) {
-  const res = []
+  const res = [];
 
-  input.data?.getFilteredVehiclesForWormwood?.vehicles?.forEach(vehicle => {
-    var tmp = {}
-    Object.keys(vehicle).forEach(key => {
+  input.data?.getFilteredVehiclesForWormwood?.vehicles?.forEach((vehicle) => {
+    var tmp = {};
+    Object.keys(vehicle).forEach((key) => {
       Object.keys(audiInventoryMapping).includes(key)
-        ? tmp[audiInventoryMapping[key]] = vehicle[key]
-        : null
-    })
+        ? (tmp[audiInventoryMapping[key]] = vehicle[key])
+        : null;
+    });
 
     // The Audi MSRP is provided as $12,345.00. Stripping the cents, and removing non-digits
-    tmp['price'] = tmp['price'].split('.')[0].replace(/\D/g, '')
+    tmp["price"] = tmp["price"].split(".")[0].replace(/\D/g, "");
 
     /**
      * Vehicles in transit have more specific availability data in 'vehicleOrderStatus'.
@@ -70,44 +81,47 @@ function formatAudiInventoryResults(input) {
      * So dealing with that here
      */
     //
-    vehicle['vehicleOrderStatus'] === null
-      ? tmp['deliveryDate'] = titleCase(vehicle['vehicleInventoryType'].replace('-', ' '))
-      : null
+    vehicle["vehicleOrderStatus"] === null
+      ? (tmp["deliveryDate"] = titleCase(
+          vehicle["vehicleInventoryType"].replace("-", " ")
+        ))
+      : null;
 
     // Populating the Availability filter
-    tmp['inventoryStatus'] = tmp['deliveryDate']
+    tmp["inventoryStatus"] = tmp["deliveryDate"];
 
-    res.push({ ...tmp, ...vehicle })
-  })
+    res.push({ ...tmp, ...vehicle });
+  });
 
-  return res
+  return res;
 }
 
 function formatAudiVinResults(input) {
-  const vinFormattedData = {}
-  const vinData = input.data.getVehicleInfoForWormwood
+  const vinFormattedData = {};
+  const vinData = input.data.getVehicleInfoForWormwood;
 
   // Replace Audi JSON keys with EV Finder JSON keys
-  Object.keys(vinData).forEach(vinKey => {
+  Object.keys(vinData).forEach((vinKey) => {
     Object.keys(audiVinMapping).includes(vinKey)
-      ? vinFormattedData[audiVinMapping[vinKey]] = vinData[vinKey]
-      : null
-  })
+      ? (vinFormattedData[audiVinMapping[vinKey]] = vinData[vinKey])
+      : null;
+  });
 
   // The dealer note is provided as raw HTML. Stripping the HTML to display plain text
-  vinFormattedData['Dealer Note'] = stripHTML(vinFormattedData['Dealer Note'])
+  vinFormattedData["Dealer Note"] = stripHTML(vinFormattedData["Dealer Note"]);
 
   // It appears for new vehicles, 'vehicleMilage' is null. Replacing with something descriptive
-  if (vinFormattedData['Vehicle Mileage'] === null) {
-    vinFormattedData['Vehicle Mileage'] = "N/A"
+  if (vinFormattedData["Vehicle Mileage"] === null) {
+    vinFormattedData["Vehicle Mileage"] = "N/A";
   }
 
   // Capitalize Market
-  vinFormattedData['Market'] = vinFormattedData['Market'].toUpperCase()
+  vinFormattedData["Market"] = vinFormattedData["Market"].toUpperCase();
 
   // Titlecase Vehicle Type
-  vinFormattedData['Vehicle Type'] = titleCase(vinFormattedData['Vehicle Type'])
+  vinFormattedData["Vehicle Type"] = titleCase(
+    vinFormattedData["Vehicle Type"]
+  );
 
-
-  return vinFormattedData
+  return vinFormattedData;
 }
