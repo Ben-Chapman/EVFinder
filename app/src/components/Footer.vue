072aa57c5eaddba778d34047ebabd78c14e48631
Ben Chapman <5124569+Ben-Chapman@users.noreply.github.com>
2023-01-01 10:35:56 -0500

    Adjusting footer for multi-year copyright (#72)

--- app/src/components/Footer.vue
+++ app/src/components/Footer.vue
@@ -1,12 +1,11 @@
 <template>
   <b-container fluid class="footer frosted-bg">
     <b-row align-h="center" class="d-flex justify-content-center pt-1 footer-text">
-      Copyright ©{{ this.currentYear }} The EV Finder | 
-      Licensed under&nbsp;
+      ©2022 &ndash; {{ this.currentYear }} The EV Finder | Licensed under&nbsp;
       <a href="https://github.com/Ben-Chapman/EVFinder/blob/main/LICENSE">
         <u>GNU GPLv3</u>
       </a>
-      </b-row>
+    </b-row>
 
       <b-row align-h="center" class="d-flex justify-content-center py-2">
         <a href="https://github.com/Ben-Chapman/EVFinder">
@@ -34,7 +33,7 @@
 
 <style lang="scss">
   @import '../assets/app_style.scss';
-  
+
   .footer {
     position: sticky;
     bottom: 0;
