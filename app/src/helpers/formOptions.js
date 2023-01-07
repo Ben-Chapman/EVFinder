e7c3170c1096e7cda866445dabb034e1b1b48f0b
Ben <5124569+Ben-Chapman@users.noreply.github.com>
2023-01-07 13:30:29 -0500

    Prettier formatting

--- app/src/helpers/formOptions.js
+++ app/src/helpers/formOptions.js
@@ -1,61 +1,59 @@
 const modelOptions = [
   {
-    label: 'Audi',
+    label: "Audi",
     options: [
-      { value: 'etron', text: 'e-tron®' },
+      { value: "etron", text: "e-tron®" },
+      { value: "etrongt", text: "e-tron® GT" },
+      { value: "q4", text: "Q4 e-tron®" },
     ],
   },
   {
-    label: 'Chevrolet',
+    label: "Chevrolet",
     options: [
-      { value: 'Bolt EUV', text: 'Bolt EUV' },
-      { value: 'Bolt EV', text: 'Bolt EV' },
+      { value: "Bolt EUV", text: "Bolt EUV" },
+      { value: "Bolt EV", text: "Bolt EV" },
     ],
   },
   {
-    label: 'Ford',
+    label: "Ford",
+    options: [{ value: "mache", text: "Mustang Mach-E" }],
+  },
+  {
+    label: "Hyundai",
     options: [
-      { value: 'mache', text: 'Mustang Mach-E' },
+      { value: "Ioniq%205", text: "Ioniq 5" },
+      { value: "Ioniq%20Phev", text: "Ioniq Plug-in Hybrid" },
+      { value: "Kona%20Ev", text: "Kona Electric" },
+      { value: "Santa%20Fe%20Phev", text: "Santa Fe Plug-in Hybrid" },
+      { value: "Tucson%20Phev", text: "Tucson Plug-in Hybrid" },
     ],
   },
   {
-    label: 'Hyundai',
+    label: "Genesis",
     options: [
-      { value: 'Ioniq%205', text: 'Ioniq 5' },
-      { value: 'Ioniq%20Phev', text: 'Ioniq Plug-in Hybrid' },
-      { value: 'Kona%20Ev', text: 'Kona Electric' },
-      { value: 'Santa%20Fe%20Phev', text: 'Santa Fe Plug-in Hybrid' },
-      { value: 'Tucson%20Phev', text: 'Tucson Plug-in Hybrid' },
+      { value: "ElectrifiedG80", text: "Electrified G80" },
+      { value: "GV60", text: "GV60" },
     ],
   },
   {
-    label: 'Genesis',
+    label: "Kia",
     options: [
-      { value: 'ElectrifiedG80', text: 'Electrified G80' },
-      { value: 'GV60', text: 'GV60' },
+      { value: "N", text: "EV6" },
+      { value: "F", text: "Niro Plug-In Hybrid" },
+      { value: "V", text: "Niro EV" },
+      { value: "T", text: "Sorento Plug-in Hybrid" },
+      { value: "R", text: "Sportage Plug-in Hybrid" },
     ],
   },
   {
-    label: 'Kia',
-    options: [
-      { value: 'N', text: 'EV6' },
-      { value: 'F', text: 'Niro Plug-In Hybrid' },
-      { value: 'V', text: 'Niro EV' },
-      { value: 'T', text: 'Sorento Plug-in Hybrid' },
-      { value: 'R', text: 'Sportage Plug-in Hybrid' },
-    ],
+    label: "Volkswagen",
+    options: [{ value: "ID.4", text: "ID.4" }],
   },
-  {
-    label: 'Volkswagen',
-    options: [
-      { value: 'ID.4', text: 'ID.4' },
-    ],
-  },
-]
+];
 
 const yearOptions = [
-  { value: '2022', text: '2022' },
-  { value: '2023', text: '2023' },
-]
+  { value: "2022", text: "2022" },
+  { value: "2023", text: "2023" },
+];
 
-export { modelOptions, yearOptions }
+export { modelOptions, yearOptions };
