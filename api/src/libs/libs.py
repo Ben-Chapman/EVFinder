f67ef7d43bd4402dfb2733dc170bd34617f22891
Ben <5124569+Ben-Chapman@users.noreply.github.com>
2023-01-10 16:06:00 -0500

    Adding timing and fire-and-forget helpers

--- api/src/libs/libs.py
+++ api/src/libs/libs.py
@@ -1,7 +1,11 @@
+import asyncio
 import json
 import logging
+import time
+
 from tokenize import String
 from flask import make_response
+from functools import partial, wraps
 
 
 def send_response(response_data, content_type, cache_control_age, status_code=200):
@@ -124,3 +128,50 @@ def flatten_api_results(input_data: str):
         return json.dumps(tmp)
     else:
         return json.dumps({})
+
+
+def timeit(function_to_time):
+    @wraps(function_to_time)
+    def timed(*args, **kw):
+
+        start_time = time.perf_counter_ns()
+        output = function_to_time(*args, **kw)
+        end_time = time.perf_counter_ns()
+        total_time = end_time - start_time
+
+        # TODO: Push this to a custom metric
+        print(f"Function {function_to_time.__name__} took {total_time/1000000:.5f} ms")
+
+        return output
+
+    return timed
+
+
+def async_timeit(function_to_time):
+    @wraps(function_to_time)
+    async def wrapper(*args, **kwargs):
+        start_time = time.perf_counter_ns()
+        try:
+            return await function_to_time(*args, **kwargs)
+        finally:
+            total_time = time.perf_counter_ns() - start_time
+            # TODO: Push this to a custom metric
+            print(
+                f"Function {function_to_time.__name__} took {total_time/1000000:.5f} ms"
+            )
+
+    return wrapper
+
+
+def fire_and_forget(f):
+    """https://dotmethod.me/posts/python-async-fire-and-forget/"""
+
+    @wraps(f)
+    def wrapped(*args, **kwargs):
+        loop = asyncio.get_event_loop()
+        if callable(f):
+            return loop.run_in_executor(None, partial(f, *args, **kwargs))
+        else:
+            raise TypeError("Task must be a callable")
+
+    return wrapped
