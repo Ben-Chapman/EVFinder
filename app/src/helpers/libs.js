e952d6288a99ec9e780c551f9fb41bfdeb534e01
Ben <5124569+Ben-Chapman@users.noreply.github.com>
2023-01-12 18:07:17 -0500

    Prettier formatting

--- app/src/helpers/libs.js
+++ app/src/helpers/libs.js
@@ -1,9 +1,9 @@
-import { camelCase, startCase } from 'lodash';
+import { camelCase, startCase } from "lodash";
 
-var flattendObj = {}
+var flattendObj = {};
 const flattenObject = (obj, keyName) => {
-  Object.keys(obj).forEach(key => {
-    var newKey = camelCase(`${keyName}-${key}`)
+  Object.keys(obj).forEach((key) => {
+    var newKey = camelCase(`${keyName}-${key}`);
     if (typeof obj[key] === "object") {
       if (obj[key] != null) {
         // Recurse
@@ -12,64 +12,60 @@ const flattenObject = (obj, keyName) => {
     } else {
       flattendObj[newKey] = obj[key];
     }
-  })
-  return flattendObj
-}
+  });
+  return flattendObj;
+};
 
 function normalizeObjectKeys(inputObject, keyMap) {
-  let tmp = {}
+  let tmp = {};
   Object.entries(keyMap).forEach(([key, value]) => {
     if (Object.keys(inputObject).includes(value)) {
-      tmp[key] = inputObject[value]
+      tmp[key] = inputObject[value];
     }
-  })
-  return { ...tmp, ...inputObject }
+  });
+  return { ...tmp, ...inputObject };
 }
 
 export default function normalizeJson(inputJson, keyMap) {
-  var result = []
-  inputJson.forEach(i => {
-    result.push(
-      normalizeObjectKeys(
-        flattenObject(i, ''),
-        keyMap
-      ))
-  })
-  return result
+  var result = [];
+  inputJson.forEach((i) => {
+    result.push(normalizeObjectKeys(flattenObject(i, ""), keyMap));
+  });
+  return result;
 }
 
 export function convertToCurrency(item) {
-  var formatter = new Intl.NumberFormat('en-US', {
-    style: 'currency',
-    currency: 'USD',
+  var formatter = new Intl.NumberFormat("en-US", {
+    style: "currency",
+    currency: "USD",
     // minimumFractionDigits must also be set when maximumFractionDigits is < 2
     // This is needed to support Safari <15 on iOS
     //https://stackoverflow.com/a/41045289
     minimumFractionDigits: 0,
     maximumFractionDigits: 0,
-  })
-  return formatter.format(item)
+  });
+  return formatter.format(item);
 }
 
 export function titleCase(item) {
-  return startCase(camelCase(item))
+  return startCase(camelCase(item));
 }
 
 export function sortObjectByKey(item) {
-  return Object.fromEntries(Object.entries(item).sort())
+  return Object.fromEntries(Object.entries(item).sort());
 }
 
 export function cl(whatToLog) {
-  if (typeof (whatToLog) === 'object' && whatToLog != null) {
-    console.log(JSON.stringify(whatToLog))
+  if (typeof whatToLog === "object" && whatToLog != null) {
+    console.log(JSON.stringify(whatToLog));
   } else {
-    console.log(whatToLog)
+    console.log(whatToLog);
   }
 }
 
 export function stripHTML(html) {
   // https://stackoverflow.com/a/47140708
-  const doc = new DOMParser().parseFromString(html, 'text/html');
+  const doc = new DOMParser().parseFromString(html, "text/html");
   return doc.body.textContent || "";
 }
 
@@ -80,21 +76,20 @@ export function generateUrlQueryParams(item, sliceLength) {
    * we have to loop through that object, and only generate query params for
    * Object keys which have a value (user selected a filter item).
    */
-  var url = new URL(window.location.href)
-  Object.keys(item)
-    .forEach(key => {
-      const queryParam = key.slice(0, sliceLength)
-      const queryValue = item[key].join(',')
+  var url = new URL(window.location.href);
+  Object.keys(item).forEach((key) => {
+    const queryParam = key.slice(0, sliceLength);
+    const queryValue = item[key].join(",");
 
-      // Only generate a query param, if the user has selected a filter item
-      if (item[key].length > 0) {
-        url.searchParams.set(queryParam, queryValue)
-      }
-      // When a user deselects a filter item, remove it from the list of query params
-      else if (item[key].length == 0 && url.searchParams.has(queryParam)) {
-        url.searchParams.delete(queryParam)
-      }
-    })
+    // Only generate a query param, if the user has selected a filter item
+    if (item[key].length > 0) {
+      url.searchParams.set(queryParam, queryValue);
+    }
+    // When a user deselects a filter item, remove it from the list of query params
+    else if (item[key].length == 0 && url.searchParams.has(queryParam)) {
+      url.searchParams.delete(queryParam);
+    }
+  });
   // Push query param changes to the URL
-  window.history.replaceState({}, '', url.search)
+  window.history.replaceState({}, "", url.search);
 }
--- app/src/manufacturers/kia.js
+++ app/src/manufacturers/kia.js
@@ -1,61 +1,67 @@
-import normalizeJson from '../helpers/libs'
-import { kiaInventoryMapping, kiaVinMapping } from './kiaMappings'
+import normalizeJson from "../helpers/libs";
+import { kiaInventoryMapping, kiaVinMapping } from "./kiaMappings";
 
-const apiBase = 'https://api.theevfinder.com'
+const apiBase = "https://api.theevfinder.com";
 
 export async function getKiaInventory(zip, year, model, radius) {
-  const response = await fetch(apiBase + '/api/inventory/kia?' + new URLSearchParams({
-    zip: zip,
-    year: year,
-    model: model,
-    radius: radius,
-  }),
-  {method: 'GET', mode: 'cors',})
+  const response = await fetch(
+    apiBase +
+      "/api/inventory/kia?" +
+      new URLSearchParams({
+        zip: zip,
+        year: year,
+        model: model,
+        radius: radius,
+      }),
+    { method: "GET", mode: "cors" }
+  );
 
   if (!response.ok) {
-    return ['ERROR', response.status, await response.text()]
+    return ["ERROR", response.status, await response.text()];
   } else {
-    var r = await response.json()  // Raw results
+    var r = await response.json(); // Raw results
   }
 
-  if ('inventoryVehicles' in r) {
-    var n = normalizeJson(r['inventoryVehicles'], kiaInventoryMapping)  // Normalized results
-    n.forEach(vehicle => {
+  if ("inventoryVehicles" in r) {
+    var n = normalizeJson(r["inventoryVehicles"], kiaInventoryMapping); // Normalized results
+    console.log(JSON.stringify(n));
+    n.forEach((vehicle) => {
       // Lookup the dealer name/address from the dealer code
-      const dCode = vehicle['dealerCode']
-      const dealerDetail = r['filterSet']['dealers'].find(dealer => dealer['code'] === dCode);
+      const dCode = vehicle["dealerCode"];
+      const dealerDetail = r["filterSet"]["dealers"].find(
+        (dealer) => dealer["code"] === dCode
+      );
 
       // Some results have a fqdn for a dealerUrl, some not. Stripping the
       // scheme, which will be re-inserted by the template
-      vehicle['dealerUrl'] = dealerDetail['url'].replace(/http(s)?:\/\//i, '')
-      vehicle['dealerName'] = dealerDetail['name']
-      vehicle['city'] = dealerDetail['location']['city']
-      vehicle['state'] = dealerDetail['location']['state']
+      vehicle["dealerUrl"] = dealerDetail["url"].replace(/http(s)?:\/\//i, "");
+      vehicle["dealerName"] = dealerDetail["name"];
+      vehicle["city"] = dealerDetail["location"]["city"];
+      vehicle["state"] = dealerDetail["location"]["state"];
 
       // Distance to 2 decimal places
-      vehicle['distance'] = parseFloat(vehicle['dealerDistance']).toFixed(2).toString()
-      
+      vehicle["distance"] = parseFloat(vehicle["dealerDistance"]).toFixed(2).toString();
+
       // Delivery Date
-      if (vehicle['status'] == 'DS') {
-        vehicle['deliveryDate'] = "Available"
-        vehicle['inventoryStatus'] = "Available"
+      if (vehicle["status"] == "DS") {
+        vehicle["deliveryDate"] = "Available";
+        vehicle["inventoryStatus"] = "Available";
+      } else if (vehicle["status"] == "IT") {
+        vehicle["deliveryDate"] = "Arriving Soon";
+        vehicle["inventoryStatus"] = "Arriving Soon";
       }
-      else if (vehicle['status'] == 'IT') {
-        vehicle['deliveryDate'] = "Arriving Soon"
-        vehicle['inventoryStatus'] = "Arriving Soon"
-      }
-      
+
       /* The Kia API data is inconsistent and some vehicles don't have a
       drivetrainDesc field (AWD/RWD), but do include this information in
       a longer string description. For these vehicles, extracting the desc
       from the string
       */
-      if (vehicle['edwTrim'].match(/RWD|AWD/) != null) {
-        vehicle['drivetrainDesc'] = vehicle['edwTrim'].match(/RWD|AWD/)[0]
+      if (vehicle["edwTrim"].match(/RWD|AWD/) != null) {
+        vehicle["drivetrainDesc"] = vehicle["edwTrim"].match(/RWD|AWD/)[0];
       } else {
-        vehicle['drivetrainDesc'] = "Unknown"
+        vehicle["drivetrainDesc"] = "Unknown";
       }
-  
+
       /**
        * Kia stores the exterior color name under a top-level object called
        * exteriorImages{}, which we need to pull out for display in the UI.
@@ -64,51 +70,52 @@ export async function getKiaInventory(zip, year, model, radius) {
        * extract the actual color name.
        */
       try {
-        const extColor = vehicle['exteriorImagesExteriorProfile'].match(/[0-9a-fA-F]{6}/)[0]
+        const extColor =
+          vehicle["exteriorImagesExteriorProfile"].match(/[0-9a-fA-F]{6}/)[0];
         // Looping through the non-flattened API response object to get exterior color
-        r['filterSet']['criteriaGroups'].forEach(group => {
-          if (group.groupName === 'Colors') {
-            group['groupCriteria'].forEach(criteria => {
-              criteria['elements'].forEach(element => {
-                if (element['baseHex'] === extColor) {
-                  vehicle['exteriorColor'] = element['name']
+        r["filterSet"]["criteriaGroups"].forEach((group) => {
+          if (group.groupName === "Colors") {
+            group["groupCriteria"].forEach((criteria) => {
+              criteria["elements"].forEach((element) => {
+                if (element["baseHex"] === extColor) {
+                  vehicle["exteriorColor"] = element["name"];
                 }
-              })
-            })
+              });
+            });
           }
-        })
+        });
       } catch {
-        vehicle['exteriorColor'] = "Unknown"
+        vehicle["exteriorColor"] = "Unknown";
       }
-      
-    })
+    });
   } else {
-    n = []
+    n = [];
   }
 
-  return n
-  }
+  return n;
+}
 
 export function getKiaVinDetail(input) {
-  /** The KIA API response contains all publically available information
+  /** The KIA API response contains all publicly available information
    * about the vehicle, so there's no additional VIN API call needed. Thus
    * storing the /inventory API data directly in the vinDetail local store.
    */
-    const k = {}
-    Object.keys(input).forEach(key => {
-      if (Object.keys(kiaVinMapping).includes(key)) {
-        k[kiaVinMapping[key]] = input[key]
+  const k = {};
+  Object.keys(input).forEach((key) => {
+    if (Object.keys(kiaVinMapping).includes(key)) {
+      k[kiaVinMapping[key]] = input[key];
+    }
+    // The Kia API returns individual elements for each feature, so
+    // concatenating into a single string for display
+    if (key.indexOf("features0Options") >= 0) {
+      // Does the key contain features0Options
+      if (k["Top Features"]) {
+        k["Top Features"] = `${k["Top Features"]}, ${input[key]}`;
+      } else {
+        k["Top Features"] = input[key];
       }
-      // The Kia API returns individual elements for each feature, so
-      // concatenating into a single string for display
-      if (key.indexOf("features0Options") >= 0) {  // Does the key contain features0Options
-        if (k['Top Features']) {
-          k['Top Features'] = `${k['Top Features']}, ${input[key]}`
-        } else {
-          k['Top Features'] = input[key]
-        }
-      }
-    })
-  
-    return k
+    }
+  });
+
+  return k;
 }
