0d7444c86f5ed695c683fafed30891a5ecf15997
Ben <5124569+Ben-Chapman@users.noreply.github.com>
2023-03-17 17:46:35 -0400

    Updating Kia to support new request helper

--- app/src/manufacturers/ford.js
+++ app/src/manufacturers/ford.js
@@ -1,146 +1,164 @@
-import normalizeJson from "../helpers/libs"
-import { convertToCurrency, sortObjectByKey, titleCase } from "../helpers/libs"
-import { fordInventoryMapping, fordVinMapping } from "./fordMappings"
+import { normalizeJson } from "../helpers/libs";
+import { convertToCurrency, sortObjectByKey, titleCase } from "../helpers/libs";
+import { fordInventoryMapping, fordVinMapping } from "./fordMappings";
 
-const apiBase = 'https://api.theevfinder.com'
+const apiBase = "https://api.theevfinder.com";
 
 export async function getFordInventory(zip, year, model, radius) {
-  const inventory = await fetch(apiBase + '/api/inventory/ford?' + new URLSearchParams({
-    zip: zip,
-    year: year,
-    model: model,
-    radius: radius,
-  }),
-  {method: 'GET', mode: 'cors',})
-  
+  const inventory = await fetch(
+    apiBase +
+      "/api/inventory/ford?" +
+      new URLSearchParams({
+        zip: zip,
+        year: year,
+        model: model,
+        radius: radius,
+      }),
+    { method: "GET", mode: "cors" }
+  );
+
   if (inventory.ok) {
-    return formatFordInventoryResults(await inventory.json())
+    return formatFordInventoryResults(await inventory.json());
   } else {
-    return ['ERROR', inventory.status, await inventory.text()]
+    return ["ERROR", inventory.status, await inventory.text()];
   }
 }
 
 export async function getFordVinDetail(dealerSlug, model, vin, year, paCode, zip) {
   if (dealerSlug === undefined) {
-    const errorMessage = `Additional information could not be be retrieved for VIN ${vin}`
-    return {'Error': errorMessage}
+    const errorMessage = `Additional information could not be be retrieved for VIN ${vin}`;
+    return { Error: errorMessage };
   } else {
-    const vinData = await fetch(apiBase + '/api/vin/ford?' + new URLSearchParams({
-      dealerSlug: dealerSlug,
-      modelSlug: `${year}-${model}`.toLowerCase(),  // 2022-mache
-      vin: vin,
-      paCode: paCode,
-      zip: zip,
-      model: model,
-      year: year
-    }),
-    {method: 'GET', mode: 'cors',})
+    const vinData = await fetch(
+      apiBase +
+        "/api/vin/ford?" +
+        new URLSearchParams({
+          dealerSlug: dealerSlug,
+          modelSlug: `${year}-${model}`.toLowerCase(), // 2022-mache
+          vin: vin,
+          paCode: paCode,
+          zip: zip,
+          model: model,
+          year: year,
+        }),
+      { method: "GET", mode: "cors" }
+    );
 
     // Get VIN detail data for a single vehicle
     if (vinData.ok) {
-      return formatFordVinResults(await vinData.json())
+      return formatFordVinResults(await vinData.json());
     } else {
-      return ['ERROR', vinData.status, await vinData.text()]
+      return ["ERROR", vinData.status, await vinData.text()];
     }
   }
 }
-
 
 function formatFordInventoryResults(input) {
   if (Object.keys(input.data.filterResults).length == 0) {
     // filterResults is empty when no vehicles are found for a given search
     // So returning an empty object so the UI displays the no vehicles found message
-    return {}
+    return {};
   }
 
   // Merging the initial inventory results with any paginated vehicle results
-  let vehicles = []
-  if ( Object.hasOwn(input, 'rdata') ) {
-    vehicles = input.data.filterResults.ExactMatch.vehicles.concat(input.rdata.filterResults.ExactMatch.vehicles)
+  let vehicles = [];
+  if (Object.hasOwn(input, "rdata")) {
+    vehicles = input.data.filterResults.ExactMatch.vehicles.concat(
+      input.rdata.filterResults.ExactMatch.vehicles
+    );
   } else {
     // No paginated inventory results
-    vehicles = input.data.filterResults.ExactMatch.vehicles
+    vehicles = input.data.filterResults.ExactMatch.vehicles;
   }
 
-  var n = normalizeJson(vehicles, fordInventoryMapping)
+  var n = normalizeJson(vehicles, fordInventoryMapping);
   /**
    * Loop through the dealer information in the returned inventory object,
    * and pull out the dealerId and dealer name. We'll use this hashmap to
    * populate information displayed in the UI
    */
-  const dealers = {}
-  input.data.filterSet.filterGroupsMap.Dealer[0].filterItemsMetadata.filterItems.forEach(dealer => {
-    dealers[dealer['value']] = {  // 'value' is the key for the dealer ID
-      'displayName': dealer['displayName'],
-      'distance': dealer['distance']
+  const dealers = {};
+  input.data.filterSet.filterGroupsMap.Dealer[0].filterItemsMetadata.filterItems.forEach(
+    (dealer) => {
+      dealers[dealer["value"]] = {
+        // 'value' is the key for the dealer ID
+        displayName: dealer["displayName"],
+        distance: dealer["distance"],
+      };
     }
-  })
-  
-  n.forEach(vehicle => {
+  );
+
+  n.forEach((vehicle) => {
     // The dealerSlug is needed for VIN detail calls. Storing here for use later
-    vehicle['dealerSlug'] = input['dealerSlug']
+    vehicle["dealerSlug"] = input["dealerSlug"];
 
     // Format the inventory status
-    vehicle['daysOnDealerLot'] > 0 ? 
-      (vehicle['deliveryDate'] = `In Stock for ${vehicle['daysOnDealerLot']} days`,
-      vehicle['inventoryStatus'] = "In Stock") 
-    : (vehicle['deliveryDate'] = "Unknown",
-      vehicle['inventoryStatus'] = "In Stock")
-    
-    
+    vehicle["daysOnDealerLot"] > 0
+      ? ((vehicle["deliveryDate"] = `In Stock for ${vehicle["daysOnDealerLot"]} days`),
+        (vehicle["inventoryStatus"] = "In Stock"))
+      : ((vehicle["deliveryDate"] = "Unknown"),
+        (vehicle["inventoryStatus"] = "In Stock"));
+
     /**
      * In testing, I've seen where the dealerId provided in an individual
      * vehicle response, doesn't match any dealerId provided by the inventory
      * API response (API error?). So catching that and falling back to deriving
      * the dealer's name from another field.
      */
-    const dealerId = vehicle['dealerPaCode']
+    const dealerId = vehicle["dealerPaCode"];
     try {
-      vehicle['distance'] = dealers[dealerId]['distance']
-      vehicle['dealerName'] = dealers[dealerId]['displayName']
+      vehicle["distance"] = dealers[dealerId]["distance"];
+      vehicle["dealerName"] = dealers[dealerId]["displayName"];
     } catch (error) {
       // /dealer/Santa-Monica-Ford-12345/model/2022-Mache/... ->
       // Santa Monica Ford 12345
-      vehicle['dealerName'] = vehicle['detailPageUrl'].split('/')[2].replaceAll('-', ' ')
-      vehicle['distance'] = "0"
+      vehicle["dealerName"] = vehicle["detailPageUrl"]
+        .split("/")[2]
+        .replaceAll("-", " ");
+      vehicle["distance"] = "0";
     }
-    
-  })
+  });
 
-  return n
+  return n;
 }
 
 function formatFordVinResults(input) {
-  const v = normalizeJson([input.data.selected], fordVinMapping)[0]
+  const v = normalizeJson([input.data.selected], fordVinMapping)[0];
   const needsCurrencyConversion = [
-    'vehiclePricingMsrpPricingBase',
-    'vehiclePricingMsrpPricingOptions',
-    'vehiclePricingDestinationDeliveryCharge',
-    'vehiclePricingMsrpPricingNetPrice'
-  ]
+    "vehiclePricingMsrpPricingBase",
+    "vehiclePricingMsrpPricingOptions",
+    "vehiclePricingDestinationDeliveryCharge",
+    "vehiclePricingMsrpPricingNetPrice",
+  ];
 
-  const vinFormattedData = {}
-  Object.keys(v).forEach(vinKey => {
+  const vinFormattedData = {};
+  Object.keys(v).forEach((vinKey) => {
     // Map Ford-specific keys to EVFinder-specific keys
-    Object.keys(fordVinMapping).includes(vinKey) ? vinFormattedData[fordVinMapping[vinKey]] = v[vinKey] : null
+    Object.keys(fordVinMapping).includes(vinKey)
+      ? (vinFormattedData[fordVinMapping[vinKey]] = v[vinKey])
+      : null;
 
     // Need to format some values to display as dollars
-    needsCurrencyConversion.includes(vinKey) ? vinFormattedData[fordVinMapping[vinKey]] = convertToCurrency(v[vinKey]) : null
-  })
+    needsCurrencyConversion.includes(vinKey)
+      ? (vinFormattedData[fordVinMapping[vinKey]] = convertToCurrency(v[vinKey]))
+      : null;
+  });
 
   // Provide dealer details
-  vinFormattedData['Dealer Address'] = `${v['dealerName']}\n${v['dealerDealerAddressStreet1']} ${v['dealerDealerAddressStreet2']} ${v['dealerDealerAddressStreet3']}\n${v['dealerAddressCity']}, ${v['dealerAddressState']} ${v['dealerAddressZipCode']}`
-  
-  vinFormattedData['Dealer Phone'] = v['dealerDealerPhone']
+  vinFormattedData[
+    "Dealer Address"
+  ] = `${v["dealerName"]}\n${v["dealerDealerAddressStreet1"]} ${v["dealerDealerAddressStreet2"]} ${v["dealerDealerAddressStreet3"]}\n${v["dealerAddressCity"]}, ${v["dealerAddressState"]} ${v["dealerAddressZipCode"]}`;
+
+  vinFormattedData["Dealer Phone"] = v["dealerDealerPhone"];
 
   // Ford exposes dealer installed accessories in nested Objects, dealing with
   // that here
-  const features = input.data.selected.vehicle.vehicleFeatures
-  Object.keys(features).forEach(key => {
+  const features = input.data.selected.vehicle.vehicleFeatures;
+  Object.keys(features).forEach((key) => {
     // WheelSize desc is a duplicate of another key, so excluding it
-    if (features[key]['displayName'] != null && key != "WheelSize") {
-      vinFormattedData[titleCase(key)] = features[key]['displayName']
+    if (features[key]["displayName"] != null && key != "WheelSize") {
+      vinFormattedData[titleCase(key)] = features[key]["displayName"];
     }
-  })
-  return sortObjectByKey(vinFormattedData)
+  });
+  return sortObjectByKey(vinFormattedData);
 }
--- app/src/manufacturers/kia.js
+++ app/src/manufacturers/kia.js
@@ -1,61 +1,60 @@
-import normalizeJson from '../helpers/libs'
-import { kiaInventoryMapping, kiaVinMapping } from './kiaMappings'
+import { normalizeJson, generateErrorMessage } from "../helpers/libs";
+import { apiRequest } from "../helpers/request";
+import { kiaInventoryMapping, kiaVinMapping } from "./kiaMappings";
 
-const apiBase = 'https://api.theevfinder.com'
-
-export async function getKiaInventory(zip, year, model, radius) {
-  const response = await fetch(apiBase + '/api/inventory/kia?' + new URLSearchParams({
-    zip: zip,
-    year: year,
-    model: model,
-    radius: radius,
-  }),
-  {method: 'GET', mode: 'cors',})
-
-  if (!response.ok) {
-    return ['ERROR', response.status, await response.text()]
-  } else {
-    var r = await response.json()  // Raw results
+export async function getKiaInventory(zip, year, model, radius, manufacturer) {
+  //eslint-ignore-line
+  try {
+    const invResponse = await apiRequest("inventory", manufacturer, 3500, [
+      ...arguments,
+    ]);
+    return formatKiaInventoryResults(invResponse);
+  } catch (error) {
+    throw generateErrorMessage(error);
   }
+}
 
-  if ('inventoryVehicles' in r) {
-    var n = normalizeJson(r['inventoryVehicles'], kiaInventoryMapping)  // Normalized results
-    n.forEach(vehicle => {
+function formatKiaInventoryResults(input) {
+  const r = input;
+  if ("inventoryVehicles" in r) {
+    var n = normalizeJson(r["inventoryVehicles"], kiaInventoryMapping); // Normalized results
+    n.forEach((vehicle) => {
       // Lookup the dealer name/address from the dealer code
-      const dCode = vehicle['dealerCode']
-      const dealerDetail = r['filterSet']['dealers'].find(dealer => dealer['code'] === dCode);
+      const dCode = vehicle["dealerCode"];
+      const dealerDetail = r["filterSet"]["dealers"].find(
+        (dealer) => dealer["code"] === dCode
+      );
 
       // Some results have a fqdn for a dealerUrl, some not. Stripping the
       // scheme, which will be re-inserted by the template
-      vehicle['dealerUrl'] = dealerDetail['url'].replace(/http(s)?:\/\//i, '')
-      vehicle['dealerName'] = dealerDetail['name']
-      vehicle['city'] = dealerDetail['location']['city']
-      vehicle['state'] = dealerDetail['location']['state']
+      vehicle["dealerUrl"] = dealerDetail["url"].replace(/http(s)?:\/\//i, "");
+      vehicle["dealerName"] = dealerDetail["name"];
+      vehicle["city"] = dealerDetail["location"]["city"];
+      vehicle["state"] = dealerDetail["location"]["state"];
 
       // Distance to 2 decimal places
-      vehicle['distance'] = parseFloat(vehicle['dealerDistance']).toFixed(2).toString()
-      
+      vehicle["distance"] = parseFloat(vehicle["dealerDistance"]).toFixed(2).toString();
+
       // Delivery Date
-      if (vehicle['status'] == 'DS') {
-        vehicle['deliveryDate'] = "Available"
-        vehicle['inventoryStatus'] = "Available"
+      if (vehicle["status"] == "DS") {
+        vehicle["deliveryDate"] = "Available";
+        vehicle["inventoryStatus"] = "Available";
+      } else if (vehicle["status"] == "IT") {
+        vehicle["deliveryDate"] = "Arriving Soon";
+        vehicle["inventoryStatus"] = "Arriving Soon";
       }
-      else if (vehicle['status'] == 'IT') {
-        vehicle['deliveryDate'] = "Arriving Soon"
-        vehicle['inventoryStatus'] = "Arriving Soon"
-      }
-      
+
       /* The Kia API data is inconsistent and some vehicles don't have a
       drivetrainDesc field (AWD/RWD), but do include this information in
       a longer string description. For these vehicles, extracting the desc
       from the string
       */
-      if (vehicle['edwTrim'].match(/RWD|AWD/) != null) {
-        vehicle['drivetrainDesc'] = vehicle['edwTrim'].match(/RWD|AWD/)[0]
+      if (vehicle["edwTrim"].match(/RWD|AWD/) != null) {
+        vehicle["drivetrainDesc"] = vehicle["edwTrim"].match(/RWD|AWD/)[0];
       } else {
-        vehicle['drivetrainDesc'] = "Unknown"
+        vehicle["drivetrainDesc"] = "Unknown";
       }
-  
+
       /**
        * Kia stores the exterior color name under a top-level object called
        * exteriorImages{}, which we need to pull out for display in the UI.
@@ -64,51 +63,52 @@ export async function getKiaInventory(zip, year, model, radius) {
        * extract the actual color name.
        */
       try {
-        const extColor = vehicle['exteriorImagesExteriorProfile'].match(/[0-9a-fA-F]{6}/)[0]
+        const extColor =
+          vehicle["exteriorImagesExteriorProfile"].match(/[0-9a-fA-F]{6}/)[0];
         // Looping through the non-flattened API response object to get exterior color
-        r['filterSet']['criteriaGroups'].forEach(group => {
-          if (group.groupName === 'Colors') {
-            group['groupCriteria'].forEach(criteria => {
-              criteria['elements'].forEach(element => {
-                if (element['baseHex'] === extColor) {
-                  vehicle['exteriorColor'] = element['name']
+        r["filterSet"]["criteriaGroups"].forEach((group) => {
+          if (group.groupName === "Colors") {
+            group["groupCriteria"].forEach((criteria) => {
+              criteria["elements"].forEach((element) => {
+                if (element["baseHex"] === extColor) {
+                  vehicle["exteriorColor"] = element["name"];
                 }
-              })
-            })
+              });
+            });
           }
-        })
+        });
       } catch {
-        vehicle['exteriorColor'] = "Unknown"
+        vehicle["exteriorColor"] = "Unknown";
       }
-      
-    })
+    });
   } else {
-    n = []
+    n = [];
   }
 
-  return n
-  }
+  return n;
+}
 
 export function getKiaVinDetail(input) {
   /** The KIA API response contains all publically available information
    * about the vehicle, so there's no additional VIN API call needed. Thus
    * storing the /inventory API data directly in the vinDetail local store.
    */
-    const k = {}
-    Object.keys(input).forEach(key => {
-      if (Object.keys(kiaVinMapping).includes(key)) {
-        k[kiaVinMapping[key]] = input[key]
+  const k = {};
+  Object.keys(input).forEach((key) => {
+    if (Object.keys(kiaVinMapping).includes(key)) {
+      k[kiaVinMapping[key]] = input[key];
+    }
+    // The Kia API returns individual elements for each feature, so
+    // concatenating into a single string for display
+    if (key.indexOf("features0Options") >= 0) {
+      // Does the key contain features0Options
+      if (k["Top Features"]) {
+        k["Top Features"] = `${k["Top Features"]}, ${input[key]}`;
+      } else {
+        k["Top Features"] = input[key];
       }
-      // The Kia API returns individual elements for each feature, so
-      // concatenating into a single string for display
-      if (key.indexOf("features0Options") >= 0) {  // Does the key contain features0Options
-        if (k['Top Features']) {
-          k['Top Features'] = `${k['Top Features']}, ${input[key]}`
-        } else {
-          k['Top Features'] = input[key]
-        }
-      }
-    })
-  
-    return k
+    }
+  });
+
+  return k;
 }
