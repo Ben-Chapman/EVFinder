9f8e7ef978c503d94e40e65cfafbb4ceb848c881
Ben <5124569+Ben-Chapman@users.noreply.github.com>
2023-05-03 12:21:46 -0400

    Adding some style to the component

--- app/src/components/Slogan.vue
+++ app/src/components/Slogan.vue
@@ -1,25 +1,62 @@
 <template>
   <b-container fluid>
     <b-row align-h="center">
-      <!-- <h1 class="slogan">The Fastest and Easiest Way To Find Your New Electric Vehicle</h1>-->
-      <h1 class="slogan">Find Your New {{ this.displayName }} With The EV Finder</h1>
+      <!-- Display on small displays (mobile portrait) -->
+      <h1
+        class="slogan-small-display text-center d-flex d-sm-none mt-0"
+        :class="sloganTextColor"
+      >
+      Find Your New {{ this.displayName }}<br>With The EV Finder
+      </h1>
+
+      <!-- Show this version on all other displays -->
+      <h1
+        class="slogan-large-display d-none d-sm-none d-md-block"
+        :class="sloganTextColor"
+      >
+      Find Your New {{ this.displayName }} With The EV Finder
+      </h1>
     </b-row>
 </b-container>
 </template>
 
 <script>
+
 export default {
   props: {
-    displayName: String
+    displayName: String,
+    textColor: String
   },
-}
 
+
+  computed: {
+    sloganTextColor() {
+      return this.textColor == "dark" ? "dark-text" : "light-text"
+    }
+  }
+}
+// console.log(this.displayName)
 </script>
 
 <style>
-.slogan {
-  color: #fff;
+.slogan-large-display {
   font-size: 2.5rem;
-  filter: drop-shadow(3px 3px 1.5px #3d3d3d);
+}
+
+.slogan-small-display {
+  font-size: 1.5rem;
+  line-height: 2rem;
+}
+
+.dark-text {
+  color: #1d1d1d;
+  backdrop-filter: blur(3px) saturate(70%);
+  padding: 0px 40px;
+}
+
+.light-text {
+  color: #fff;
+  filter: drop-shadow(0px 5px 8px #3d3d3d);
+
 }
 </style>
