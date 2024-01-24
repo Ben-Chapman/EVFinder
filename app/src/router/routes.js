d85f3b710d0d6d845bc008d121f9d6c2e5149903
Ben <5124569+Ben-Chapman@users.noreply.github.com>
2024-01-24 16:17:01 -0500

    Removing unused routes

--- app/src/router/routes.js
+++ app/src/router/routes.js
@@ -8,22 +8,20 @@ const routes = [
   {
     path: "/",
     component: App,
-    props: (route) => ({
-      // These are the available query params
-      zipcode: route.query.z, // ?z=90210
-      year: route.query.y, // ?y=2022
-      model: route.query.m,
-      radius: route.query.r,
-    }),
+    // props: (route) => ({
+    //   // These are the available query params
+    //   // zipcode: route.query.z, // ?z=90210
+    //   // year: route.query.y, // ?y=2022
+    //   // model: route.query.m,
+    //   // radius: route.query.r,
+    // }),
   },
   {
     path: "/inventory/:year/:manufacturer/:model",
     component: App,
     props: (route) => ({
       // These are the available query params
-      zipcode: route.query.zipcode, // ?z=90210
-      year: route.query.year, // ?y=2022
-      model: route.query.model,
+      zipcode: route.query.zipcode,
       radius: route.query.radius,
     }),
   },
