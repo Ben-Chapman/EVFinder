6c7073122f4c8052f77c415e11b7f1faa8f9b37a
Ben <5124569+Ben-Chapman@users.noreply.github.com>
2024-02-12 11:20:50 -0500

    Initial commit

---
+++ app/generateSiteMap.js
@@ -0,0 +1,21 @@
+import { modelOptions, yearOptions } from "./src/helpers/formOptions";
+
+const lastMod = new Date().toISOString().slice(0, 10);
+
+console.log(`
+<?xml version="1.0" encoding="utf-8" standalone="yes" ?>
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+`);
+yearOptions.forEach((year) => {
+  modelOptions.forEach((m) => {
+    m.options.forEach((option) => {
+      console.log(`
+<url>
+  <loc>https://theevfinder.com/inventory/${year.value}/${option.value}/
+  <lastmod>${lastMod}</lastmod>
+</url>`);
+    });
+  });
+});
+
+console.log("</urlset>");
