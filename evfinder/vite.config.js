cce2c382bfffa6e538e2cbd443f6ed64d0166109
Ben <5124569+Ben-Chapman@users.noreply.github.com>
2024-05-15 09:15:11 -0400

    Installing Vuetify

--- evfinder/vite.config.js
+++ evfinder/vite.config.js
@@ -1,18 +1,17 @@
-import { fileURLToPath, URL } from 'node:url'
+import { fileURLToPath, URL } from "node:url";
 
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import vueJsx from '@vitejs/plugin-vue-jsx'
-import VueDevTools from 'vite-plugin-vue-devtools'
-
-import vuetify from 'vite-plugin-vuetify'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import vueJsx from "@vitejs/plugin-vue-jsx";
+import VueDevTools from "vite-plugin-vue-devtools";
+import vuetify from "vite-plugin-vuetify";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), vueJsx(), VueDevTools(), vuetify({ autoImport: true })],
+  plugins: [vue(), vueJsx(), VueDevTools(), vuetify({ autoImport: true })], // Enabled by default],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  }
-})
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
+    },
+  },
+});
