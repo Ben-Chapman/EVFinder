2f2dbff15ac9d9f5806c12b475e65c19c8873cd7
Ben <5124569+Ben-Chapman@users.noreply.github.com>
2024-05-15 09:14:41 -0400

    Initial migration to Vuetify

--- evfinder/src/components/Footer.vue
+++ evfinder/src/components/Footer.vue
@@ -1,6 +1,6 @@
 <template>
-  <b-container fluid class="footer frosted-bg">
-    <b-row align-h="center" class="d-flex justify-content-center py-2 footer-text">
+  <v-container fluid class="footer frosted-bg">
+    <v-row align="center" justify="center" class="d-flex py-2 footer-text">
       <span>©2022 &ndash; {{ this.currentYear }} The EV Finder</span>
 
       <span>
@@ -13,37 +13,37 @@
           <u>GNU GPLv3</u>
         </a>
       </span>
-    </b-row>
-  </b-container>
+    </v-row>
+  </v-container>
 </template>
 
 <script>
-import GithubLogo from './GithubLogo.vue'
-export default {
-  components: { GithubLogo },
-  computed: {
-    currentYear() {
-      return new Date().getFullYear()
-    }
-  } //computed
-}
+  import GithubLogo from "./GithubLogo.vue";
+  export default {
+    components: { GithubLogo },
+    computed: {
+      currentYear() {
+        return new Date().getFullYear();
+      },
+    }, //computed
+  };
 </script>
 
 <style lang="scss">
-@import '../assets/app_style.scss';
+  // @import '../assets/app_style.scss';
 
-.footer {
-  position: sticky;
-  bottom: 0;
-  height: auto;
-}
+  .footer {
+    position: sticky;
+    bottom: 0;
+    height: auto;
+  }
 
-.footer-text a {
-  color: black;
-}
+  .footer-text a {
+    color: black;
+  }
 
-.frosted-bg {
-  background-color: rgba(255, 255, 255, 0.15);
-  backdrop-filter: blur(10px);
-}
+  .frosted-bg {
+    background-color: rgba(255, 255, 255, 0.15);
+    backdrop-filter: blur(10px);
+  }
 </style>
--- evfinder/src/components/GithubLogo.vue
+++ evfinder/src/components/GithubLogo.vue
@@ -1,19 +1,22 @@
 <template>
-  <b-container>
-    <b-row align-h="center" class="d-flex justify-content-center">
+  <v-container>
+    <v-row align-h="center" class="d-flex justify-content-center">
       <a target="_blank" href="https://github.com/Ben-Chapman/EVFinder">
-        <b-icon-github aria-hidden="true" class="icon" font-scale="1.3"> </b-icon-github>
+        <!-- <b-icon-github aria-hidden="true" class="icon" font-scale="1.3">
+        </b-icon-github> -->
+        <v-icon color="info" icon="mdi-check-circle" size="large"></v-icon>
       </a>
-    </b-row>
-  </b-container>
+    </v-row>
+  </v-container>
 </template>
-<style lang="scss">
-@import '../assets/app_style.scss';
-.icon {
-  color: #000;
-}
 
-.icon:hover {
-  color: $analogous-logo-green !important;
-}
+<style lang="scss">
+  // @import '../assets/app_style.scss';
+  .icon {
+    color: #000;
+  }
+
+  .icon:hover {
+    // color: $analogous-logo-green !important;
+  }
 </style>
