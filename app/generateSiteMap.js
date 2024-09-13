c9313c8abd7b734b013e904afaafb6afe0b66981
Ben <5124569+Ben-Chapman@users.noreply.github.com>
2024-09-12 21:05:24 -0400

    Fix import error

--- app/generateSiteMap.js
+++ app/generateSiteMap.js
@@ -1,21 +1,21 @@
-import { modelOptions, yearOptions } from "./src/helpers/formOptions";
+import { modelOptions, yearOptions } from './src/helpers/formOptions.js'
 
-const lastMod = new Date().toISOString().slice(0, 10);
+const lastMod = new Date().toISOString().slice(0, 10)
 
 console.log(`<?xml version="1.0" encoding="utf-8" standalone="yes" ?>
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-`);
+`)
 yearOptions.forEach((year) => {
   modelOptions.forEach((m) => {
-    const manufacturer = m.label.toLowerCase();
+    const manufacturer = m.label.toLowerCase()
     m.options.forEach((option) => {
       console.log(`
 <url>
   <loc>https://theevfinder.com/inventory/${year.value}/${manufacturer}/${option.value}/</loc>
   <lastmod>${lastMod}</lastmod>
-</url>`);
-    });
-  });
-});
+</url>`)
+    })
+  })
+})
 
-console.log("</urlset>");
+console.log('</urlset>')
