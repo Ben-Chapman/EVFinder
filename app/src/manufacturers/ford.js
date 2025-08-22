8d442cdd680283d63f56be7c5ec8e5bdd0f0b069
Ben <5124569+Ben-Chapman@users.noreply.github.com>
2025-08-22 16:33:26 -0400

    Add no inventory message for Ford

--- app/src/manufacturers/ford.js
+++ app/src/manufacturers/ford.js
@@ -19,25 +19,34 @@ import { apiRequest } from "../helpers/request";
 import {
   convertToCurrency,
   generateErrorMessage,
+  generateInfoMessage,
   normalizeJson,
   sortObjectByKey,
   titleCase,
 } from "../helpers/libs";
-import { fordInventoryMapping, fordVinMapping } from "./fordMappings";
+import { fordVinMapping } from "./fordMappings";
 
+// eslint-disable-next-line
 export async function getFordInventory(zip, year, model, radius, manufacturer) {
-  try {
-    const invResponse = await apiRequest(
-      "inventory",
-      manufacturer,
-      [...arguments],
-      [],
-      45000,
-    );
-    return formatFordInventoryResults(invResponse, year);
-  } catch (error) {
-    throw generateErrorMessage(error);
-  }
+  return generateInfoMessage(
+    "Inventory Not Available",
+    `Inventory information for the ${year} Ford ${titleCase(
+      model,
+    )} is not available at this time.<br><br>Please visit <a href="https://www.ford.com/finder/2024/f150-lightning" target="_blank">https://shop.ford.com</a> for more detail.`,
+  );
+
+  // try {
+  //   const invResponse = await apiRequest(
+  //     "inventory",
+  //     manufacturer,
+  //     [...arguments],
+  //     [],
+  //     45000,
+  //   );
+  //   return formatFordInventoryResults(invResponse, year);
+  // } catch (error) {
+  //   throw generateErrorMessage(error);
+  // }
 }
 
 export async function getFordVinDetail(
@@ -69,89 +78,89 @@ export async function getFordVinDetail(
   }
 }
 
-function formatFordInventoryResults(input, year) {
-  if (Object.keys(input.data.filterResults).length == 0) {
-    // filterResults is empty when no vehicles are found for a given search
-    // Returning an empty object so the UI displays the no vehicles found message
-    return {};
-  }
+// function formatFordInventoryResults(input, year) {
+//   if (Object.keys(input.data.filterResults).length == 0) {
+//     // filterResults is empty when no vehicles are found for a given search
+//     // Returning an empty object so the UI displays the no vehicles found message
+//     return {};
+//   }
 
-  let vehicles = input.data.filterResults.ExactMatch.vehicles;
-  let d =
-    input.data.filterSet.filterGroupsMap.Dealer[0].filterItemsMetadata.filterItems;
+//   let vehicles = input.data.filterResults.ExactMatch.vehicles;
+//   let d =
+//     input.data.filterSet.filterGroupsMap.Dealer[0].filterItemsMetadata.filterItems;
 
-  // rdata is returned from the EV Finder API when it has to page to retrieve all available
-  // inventory information for a given search. Here we're merging the initial inventory
-  // results with any paginated inventory results
-  if (Object.hasOwn(input, "rdata")) {
-    Object.keys(input.rdata).forEach((key) => {
-      if (key == "vehicles") {
-        // Merging vehicle information
-        input.rdata[key].forEach((vehiclePaginationResult) => {
-          vehicles = vehicles.concat(vehiclePaginationResult);
-        });
-      } else {
-        // Merging dealer information
-        input.rdata[key].forEach((dealerPaginationResult) => {
-          d = d.concat(dealerPaginationResult);
-        });
-      }
-    });
-  }
+//   // rdata is returned from the EV Finder API when it has to page to retrieve all available
+//   // inventory information for a given search. Here we're merging the initial inventory
+//   // results with any paginated inventory results
+//   if (Object.hasOwn(input, "rdata")) {
+//     Object.keys(input.rdata).forEach((key) => {
+//       if (key == "vehicles") {
+//         // Merging vehicle information
+//         input.rdata[key].forEach((vehiclePaginationResult) => {
+//           vehicles = vehicles.concat(vehiclePaginationResult);
+//         });
+//       } else {
+//         // Merging dealer information
+//         input.rdata[key].forEach((dealerPaginationResult) => {
+//           d = d.concat(dealerPaginationResult);
+//         });
+//       }
+//     });
+//   }
 
-  var n = normalizeJson(vehicles, fordInventoryMapping);
-  /**
-   * Loop through the dealer information in the returned inventory object,
-   * and pull out the dealerId and dealer name. We'll use this hashmap to
-   * populate information displayed in the UI
-   */
-  const dealers = {};
+//   var n = normalizeJson(vehicles, fordInventoryMapping);
+//   /**
+//    * Loop through the dealer information in the returned inventory object,
+//    * and pull out the dealerId and dealer name. We'll use this hashmap to
+//    * populate information displayed in the UI
+//    */
+//   const dealers = {};
 
-  d.forEach((dealer) => {
-    dealers[dealer["value"]] = {
-      // 'value' is the key for the dealer ID
-      displayName: dealer["displayName"],
-      distance: dealer["distance"],
-    };
-  });
+//   d.forEach((dealer) => {
+//     dealers[dealer["value"]] = {
+//       // 'value' is the key for the dealer ID
+//       displayName: dealer["displayName"],
+//       distance: dealer["distance"],
+//     };
+//   });
 
-  n.forEach((vehicle) => {
-    // The dealerSlug is needed for VIN detail calls. Storing here for use later
-    vehicle["dealerSlug"] = input["dealerSlug"];
+//   n.forEach((vehicle) => {
+//     // The dealerSlug is needed for VIN detail calls. Storing here for use later
+//     vehicle["dealerSlug"] = input["dealerSlug"];
 
-    // Format the inventory status
-    vehicle["daysOnDealerLot"] > 0
-      ? ((vehicle["deliveryDate"] = `In Stock for ${vehicle["daysOnDealerLot"]} days`),
-        (vehicle["inventoryStatus"] = "In Stock"))
-      : ((vehicle["deliveryDate"] = "Unknown"),
-        (vehicle["inventoryStatus"] = "In Stock"));
+//     // Format the inventory status
+//     vehicle["daysOnDealerLot"] > 0
+//       ? ((vehicle["deliveryDate"] = `In Stock for ${vehicle["daysOnDealerLot"]} days`),
+//         (vehicle["inventoryStatus"] = "In Stock"))
+//       : ((vehicle["deliveryDate"] = "Unknown"),
+//         (vehicle["inventoryStatus"] = "In Stock"));
 
-    /**
-     * In testing, I've seen where the dealerId provided in an individual
-     * vehicle response, doesn't match any dealerId provided by the inventory
-     * API response (API error?). So catching that and falling back to deriving
-     * the dealer's name from another field.
-     */
-    const dealerId = vehicle["dealerPaCode"];
-    try {
-      vehicle["distance"] = dealers[dealerId]["distance"];
-      vehicle["dealerName"] = dealers[dealerId]["displayName"];
-    } catch (error) {
-      // /dealer/Santa-Monica-Ford-12345/model/2022-Mache/ -> Santa Monica Ford 12345
-      vehicle["dealerName"] = vehicle["detailPageUrl"]
-        .split("/")[2]
-        .replaceAll("-", " ");
-      vehicle["distance"] = "0";
-    }
-  });
+//     /**
+//      * In testing, I've seen where the dealerId provided in an individual
+//      * vehicle response, doesn't match any dealerId provided by the inventory
+//      * API response (API error?). So catching that and falling back to deriving
+//      * the dealer's name from another field.
+//      */
+//     const dealerId = vehicle["dealerPaCode"];
+//     try {
+//       vehicle["distance"] = dealers[dealerId]["distance"];
+//       vehicle["dealerName"] = dealers[dealerId]["displayName"];
+//     } catch (error) {
+//       // /dealer/Santa-Monica-Ford-12345/model/2022-Mache/ -> Santa Monica Ford 12345
+//       vehicle["dealerName"] = vehicle["detailPageUrl"]
+//         .split("/")[2]
+//         .replaceAll("-", " ");
+//       vehicle["distance"] = "0";
+//     }
+//   });
 
-  /**
-   * If no vehicles are found for a given model year the Ford API will return inventory
-   * for the current model year. After receiving the API response we need to filter it
-   * so we only return results for the requested model year
-   */
-  return n.filter((vehicle) => vehicle.year == year);
-}
+//   /**
+//    * If no vehicles are found for a given model year the Ford API will return inventory
+//    * for the current model year. After receiving the API response we need to filter it
+//    * so we only return results for the requested model year
+//    */
+//   return n.filter((vehicle) => vehicle.year == year);
+// }
 
 function formatFordVinResults(input) {
   const v = normalizeJson([input.data.selected], fordVinMapping)[0];
