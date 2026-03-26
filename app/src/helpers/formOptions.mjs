f2701b3144d5dd008c6adff99238ee884a0d9136
Ben <5124569+Ben-Chapman@users.noreply.github.com>
2026-03-26 08:43:13 -0400

    Add support for 2027 model year

--- app/src/helpers/formOptions.mjs
+++ app/src/helpers/formOptions.mjs
@@ -129,6 +129,7 @@ const yearOptions = [
   { value: "2024", text: "2024" },
   { value: "2025", text: "2025" },
   { value: "2026", text: "2026" },
+  { value: "2027", text: "2027" },
 ];
 
 export { modelOptions, yearOptions };
